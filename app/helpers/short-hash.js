e7396ce977421c4809e4965331d4d933a867b0a4
Stephan Florquin <stephan.florquin@gmail.com>
2016-07-28 11:24:54 +0200

    Change short hash length

--- app/helpers/short-hash.js
+++ app/helpers/short-hash.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function shortHash([hash]) {
-  return `${hash.substr(0, 8)}..${hash.substr(hash.length - 8)}`;
+  return `${hash.substr(0, 8)}..${hash.substr(hash.length - 2)}`;
 }
 
 export default Ember.Helper.helper(shortHash);
